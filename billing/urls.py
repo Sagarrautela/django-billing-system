74f36c05306056848c85d88bb1ade4d2b94d5eaf
sagar rautela <sagarrautela1999@gmail.com>
2024-04-10 14:59:07 +0530

    set swagger url as default

--- billing/urls.py
+++ billing/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include('authentication.urls')),
     path('api/', include('product_management.urls')),
     path('api/', include('customer_management.urls')),
@@ -41,6 +41,6 @@ urlpatterns = [
 
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
