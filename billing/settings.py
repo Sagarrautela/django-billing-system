1da97cbf1ee4074b393bcf147081691373603894
sagar rautela <sagar.singh@technocratshorizons.com>
2024-04-10 14:49:21 +0530

    update swagger settings

--- billing/settings.py
+++ billing/settings.py
@@ -152,3 +152,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Swagger
+#
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
